f9b0b1e85ab5bc7d1c8e6882e1edcf9d3434e02b
eonion <tkddjsdl33@kakao.com>
2024-08-23 23:48:47 +0900

    Study: val, var

--- src/main/kotlin/com/example/studyKotlin/StudyKotlinApplication.kt
+++ src/main/kotlin/com/example/studyKotlin/StudyKotlinApplication.kt
@@ -1,3 +1,5 @@
+@file:Suppress("UNUSED_EXPRESSION")
+
 package com.example.studyKotlin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -18,6 +20,10 @@ fun main() {
     println(max2(1, 2))
     println(max3(1, 2))
 
+    println("valMessage: ${valAndVar("S")}")
+    println("valMessage: ${valAndVar("F")}")
+
+    println(valRef())
 }
 
 data class Person(
@@ -33,3 +39,40 @@ fun max2(a: Int, b: Int): Int = if (a > b) a else b
 
 // 정적 타입 지정 언어인 코틀린 -> 컴파일 시점에 반환 타입을 지정해야하지만 식이 본문인 경우, 컴파일러가 본문을 해석하여 식의 결과 타입을 지정 -> 타입 추론
 fun max3(a: Int, b: Int) = if (a > b) a else b
+
+fun valAndVar(flag: String): String {
+    // val(value) -> immutable(자바의 final)
+    // var(variable) -> mutable
+
+    // val는 한번만 초기화 가능함 블록에 따라 컴파일러가 한번만 초기화
+    val message1: String
+    var message2: String = "default"
+    println("varMessage $message2")
+
+    if (canPerformOperation(flag)) {
+        message1 = "Success"
+        message2 = "Success"
+        println("varMessage = $message2")
+    } else {
+        message1 = "Failed"
+        message2 = "Failed"
+        println("varMessage = $message2")
+    }
+    return message1
+}
+
+fun canPerformOperation(flag: String): Boolean {
+    return if (flag == "S") true else false
+}
+
+fun valRef(): Unit {
+    // val이지만 참조가 가리키는 객체 내부는 변경 가능
+    val lang = arrayListOf("Java")
+    lang.add("Kotlin")
+    println("$lang")
+}
+fun varRef() {
+    var answer = 10
+//    answer = "TEST"
+    //Type mismatch: inferred type is String but Int was expected
+}
