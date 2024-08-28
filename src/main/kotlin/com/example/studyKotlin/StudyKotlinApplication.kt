6e5faacf15e5893c0d048078059ddb66fc20c449
eonion <tkddjsdl33@kakao.com>
2024-08-28 22:53:13 +0900

    Study: When

--- src/main/kotlin/com/example/studyKotlin/StudyKotlinApplication.kt
+++ src/main/kotlin/com/example/studyKotlin/StudyKotlinApplication.kt
@@ -2,12 +2,10 @@
 
 package com.example.studyKotlin
 
+import com.example.studyKotlin.Color.*
 import com.example.studyKotlin.domain.Person
 import com.example.studyKotlin.domain.Rectangle
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.domain.EntityScan
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
-import org.springframework.boot.runApplication
 
 @SpringBootApplication
 //@EnableJpaAuditing
@@ -44,8 +42,22 @@ fun main(args: Array<String>) {
 
     println(Color.BLUE.rgb())
     println(Color.GREEN)
+    println(getMnemonic(Color.BLUE))
+    println(getWarmth(BLUE))
 
+    println(mix(RED, YELLOW))
+    println(mix(YELLOW, RED))
+//    println(mix(BLUE, GREEN))
+
+    println(mixOptimized(RED, YELLOW))
+    println(mixOptimized(YELLOW, RED))
+//    println(mixOptimized(BLUE, GREEN))
+
+    println(eval(Sum(Sum(Num(1), Num(2)), Num(4))))
+    println(evalOptimized(Sum(Sum(Num(1), Num(2)), Num(4))))
+    println(evalWithLogging(Sum(Sum(Num(1), Num(2)), Num(4))))
 }
+
 
 data class Person1(
     val name: String,
@@ -110,3 +122,80 @@ fun stringTem(args: Array<String>) {
     println("\${name}, $name!")
     println("\${if}:, ${if (args.isNotEmpty()) args[0] else "Kotlin"}")
 }
+
+// Java Switch
+// break (X)
+fun getMnemonic(c: Color) =
+    when (c) {
+        Color.RED -> "Richard"
+        Color.ORANGE -> "Of"
+        Color.YELLOW -> "York"
+        Color.GREEN -> "Gave"
+        Color.BLUE -> "Battle"
+        Color.INDIGO -> "In"
+        Color.VIOLET -> "Vain"
+    }
+
+fun getWarmth(c: Color) =
+    when (c) {
+        RED, ORANGE, YELLOW -> "warm"
+        GREEN -> "natural"
+        BLUE, INDIGO, VIOLET -> "cold"
+    }
+
+// set 표준 라이브러리 -> 순서 X
+fun mix(c1: Color, c2: Color) =
+    when (setOf(c1, c2)) {
+        setOf(RED, YELLOW) -> ORANGE
+        setOf(YELLOW, BLUE) -> GREEN
+        setOf(BLUE, VIOLET) -> INDIGO
+        else -> throw Exception("Dirty Color")
+    }
+
+fun mixOptimized(c1: Color, c2: Color) =
+    when {
+        (c1 == RED && c2 == YELLOW) || (c1 == YELLOW && c2 == RED) -> ORANGE
+        (c1 == YELLOW && c2 == BLUE) || (c1 == BLUE && c2 == YELLOW) -> GREEN
+        (c1 == BLUE && c2 == VIOLET) || (c1 == VIOLET && c2 == BLUE) -> INDIGO
+        else -> throw Exception("Dirty Color")
+    }
+
+interface Expr
+class Num(val value: Int) : Expr
+class Sum(val left: Expr, val right: Expr) : Expr
+
+
+fun eval(e: Expr): Int {
+    // java에서 instanceOf 같은 타입 검사
+    if (e is Num) {
+        // as Num으로 타입 캐스팅이 자동으로 된다.(is로 검사했으니까 컴파일러가 자동으로 스마트 캐스팅함)
+        // 변수는 반드시 val(immutable), 커스텀 접근자 (X)
+        val n = e as Num
+        return n.value
+    }
+    if (e is Sum) return eval(e.right) + eval(e.left)
+    throw IllegalArgumentException("Unknown expression")
+}
+
+fun evalOptimized(e: Expr) : Int =
+    when (e) {
+        is Num -> e.value
+        is Sum -> evalOptimized(e.left) + evalOptimized(e.right)
+        else -> throw IllegalArgumentException("Unknown expression")
+    }
+
+fun evalWithLogging(e: Expr): Int =
+    when (e) {
+        is Num -> {
+            println("num: ${e.value}")
+            e.value
+        }
+        is Sum -> {
+            val left = evalWithLogging(e.left)
+            val right = evalWithLogging(e.right)
+            println("sum: $left + $right")
+            // 블록 마지막이 반환 값이 됨 -> scope 함수 let? 이랑 비슷하게 생각하면 될듯
+            left + right
+        }
+        else -> throw IllegalArgumentException("Unknown expression")
+    }
