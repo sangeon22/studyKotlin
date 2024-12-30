cd88d453b4405173e49194eb7f331530a4c38fee
eonion <tkddjsdl33@kakao.com>
2024-12-30 23:09:22 +0900

    Study: open, final, abstract 변경자

--- src/main/kotlin/com/example/studyKotlin/domain/Click.kt
+++ src/main/kotlin/com/example/studyKotlin/domain/Click.kt
@@ -39,3 +39,27 @@ class ClickButton :Clickable, Focusable{
 //     상속한 구현 중 하나만 호출해도 되면 아래처럼 가능
 //    override fun showOff() = super<Clickable>.showOff()
 }
+
+// 코틀린에서 클래스와 메서드는 final default
+// open 키워드로 상속 가능을 명시
+open class RichButton : Clickable {
+
+    // final method
+    fun disable() {}
+
+    // final method x
+    open fun animate() {}
+
+    // override method는 기본적으로 open 상태이므로 final을 선언하여 하위 클래스에서 오버라이드 금지
+    final override fun click() {}
+}
+
+// 추상 클래스는 인스턴스화 X -> 하위 클래스에서 추상 메서드를 오버라이드 해야 한다 그러므로 open이 default
+abstract class Animated {
+    abstract fun animate()
+
+    // 비추상 메서드도 open 가능
+    open fun stopAnimating() {}
+
+    fun animateTwice() {}
+}
