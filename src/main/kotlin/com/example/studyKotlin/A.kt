4d775ae4c19cfa3c62744e5debdca18644f62ed8
eonion <tkddjsdl33@kakao.com>
2025-03-10 22:46:48 +0900

    Study: Companion Object2
    팩토리 메서드, 정적 멤버 위치

--- src/main/kotlin/com/example/studyKotlin/A.kt
+++ src/main/kotlin/com/example/studyKotlin/A.kt
@@ -25,6 +25,9 @@ class User1 {
 }
 
 // TO-BE: 부생성자를 팩토리 메서드로 개선
+// 인스턴스화 없이 즉 객체를 생성하지 않고 접근할 수 있는 변수, 메서드 선언
+// java -> static 정적 멤버 선언
+// kotlin -> companion object 동반 객체 선언
 class User2 private constructor(val nickName: String) { // 주생성자를 private으로
     companion object {
         fun newSubscribingUser(email: String) = User2(email.substringBefore("@"))
